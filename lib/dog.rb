cd3ba5e9a3c15f99f05680ebcc021ae559958633
HANA LEANDER <hana0070@gmail.com>
2019-04-23 18:42:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,18 @@
 class Dog
+  def name
+    @name
+  end
 
   def name=(name)
     @name = name
   end
 
-  def name
-    @name
+  def breed
+    @breed
   end
 
   def breed=(breed)
     @breed = breed
   end
 
-  def breed
-    @breed
-  end
 end
